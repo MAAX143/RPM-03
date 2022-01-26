0e082bd13bdb62f6178f0098c879d2a34f86b6ef
MAAX143 <makshrenycev@gmail.com>
2022-01-26 16:22:53 +0300

    Гипотеза Коллатца.

--- main.py
+++ main.py
@@ -7,5 +7,14 @@ def x2():
 def x3_1():
     pass #Кирилл
 
-def collatz():
-    pass #Макс
+def collatz(x):
+    if x == 1:
+        sp.append(x)
+        return x
+    elif x % 2 == 0:
+        return x2(x)
+    else:
+        return x3_1(x)
+
+print ('Список имеет вид: '+str(x))
+sp = []
