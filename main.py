7e6b091a9d5df277b0ca539710fbf2a77a05091b
Kirleoke <shalugin.kirillkpc2442020@gmail.com>
2022-01-26 20:47:47 +0300

    Функция x*3+1

--- main.py
+++ main.py
@@ -4,8 +4,10 @@ def input_valid():
 def x2():
     pass #Саня
 
-def x3_1():
-    pass #Кирилл
+def x3_1(x):
+    x = x * 3 + 1
+    sp.append(int(x))
+    return callatz(x)
 
 def collatz():
     pass #Макс
