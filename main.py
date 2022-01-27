f856ccd874db21b807480a31d415ea83dc414804
pomaxam <rvarfikov@gmail.com>
2022-01-27 22:07:10 +0300

    Ввод числа x, ver 3

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ def input_valid():
     while True:
         x = int(input("Введите переменную x = "))
         if x > 0:
+            collatz(x)
             break
         print("Неверное число, попробуйте другое")
     return x
