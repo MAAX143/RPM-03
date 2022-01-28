e7ba220c1623355260b6c72938744d424531c5a4
Tesl1k <danildavydov161@mail.ru>
2022-01-28 09:21:47 +0300

    dev

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ def input_valid():
     while True:
         x = int(input("Введите переменную x = "))
         if x > 0:
+            sp.append(x)
             collatz(x)
+
             break
         print("Неверное число, попробуйте другое")
     return x
@@ -18,18 +20,13 @@ def x3_1(x):
     return collatz(x)
 
 def collatz(x):
-    if x in sp:
-        print ('Значение уже есть в списке')
-        return input_valid()
     while x != 1:
-        sp.append(x)
-        return x
-    elif x % 2 == 0:
-        return x2(x)
-    else:
-        return x3_1(x)
-
+        if x % 2 == 0:
+            return x2(x)
+        else:
+            return x3_1(x)
+    print('Список имеет вид: ', sp)
 sp = []
 input_valid()
-print ('Список имеет вид: '+str(x))
+
 
